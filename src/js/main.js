98b0ba79dc23558713983ea8fc7f214e7a8f0ddf
Svetlana Berdnikova <sveta0811b@mail.ru>
2026-02-19 21:29:53 +0300

    Made open mobile-menu

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,10 @@
 const btn = document.querySelectorAll('.btn')
 const modalForm = document.querySelector('.modal')
 const modalCross = document.querySelector('.cross_modal')
+const burgerMenu = document.querySelector('.bar')
+const mobileMenu = document.querySelector('.menu-mobile')
+const menuExit = document.querySelector('.cross')
+const headerMain = document.querySelector('.header-top')
 
 btn.forEach(button => {
 button.addEventListener('click', (event) => {
@@ -12,5 +16,20 @@ button.addEventListener('click', (event) => {
 modalCross.addEventListener('click', (event) => {
     modalForm.style.display = 'none';
 });
+
+burgerMenu.addEventListener ('click', (event) => {
+    mobileMenu.style.height = 'max-content'
+
+});
+
+menuExit.addEventListener('click', (event) => {
+    mobileMenu.style.height = '0%'
+});
+
+
+
+
+
+
 
 
