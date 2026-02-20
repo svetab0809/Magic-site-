38a188e54a4b97120b5a5a4608b6d5a410d2e2b7
Svetlana Berdnikova <sveta0811b@mail.ru>
2026-02-20 15:31:25 +0300

    Made slider for reviews

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,41 @@ const mobileMenu = document.querySelector('.menu-mobile')
 const menuExit = document.querySelector('.cross')
 const headerMain = document.querySelector('.header-top')
 
+
+// const priceSlider = new Swiper ('.services', {
+//
+//     navigation: {
+//         prevEl: "#price-prev",
+//         nextEl: "#price-next",
+//     }
+//
+// })
+
+const reviewsSlider = new Swiper (".reviews", {
+
+    navigation: {
+        prevEl: "#reviews-prev",
+        nextEl: "#reviews-next",
+    },
+
+    simulateTouch: true,
+    touchRatio:1,
+    touchAngle:45,
+    grabCursor:true,
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 150,
+
+    breakpoints: {
+
+        1440: {spaceBetween: 120},
+        768: {spaceBetween: 100},
+        540: {spaceBetween: 50},
+    }
+});
+
+
+
 btn.forEach(button => {
 button.addEventListener('click', (event) => {
     modalForm.style.display = 'block'
@@ -18,13 +53,15 @@ modalCross.addEventListener('click', (event) => {
 });
 
 burgerMenu.addEventListener ('click', (event) => {
-    mobileMenu.style.height = 'max-content'
+    mobileMenu.style.height = '100%'
 
 });
 
 menuExit.addEventListener('click', (event) => {
     mobileMenu.style.height = '0%'
 });
+
+
 
 
 
